cc5b2b9c699e1ff1b6e6a9116e057d16d3f440ff
John Valentine <j5v@users.noreply.github.com>
2024-02-25 23:11:11 +0000

    Fix failure for secure stylesheets
    Firefox disallowed changes to a CSS stylesheet downloaded securely, and caused the app to fail before starting. This code change means the app can run, but with degraded performance.

--- app.js
+++ app.js
@@ -1092,30 +1092,34 @@ app = () => {
   function adjustCSSRules(selector, props, sheets){
     // from: https://jsfiddle.net/khrvndua/2/
     
-    // get stylesheet(s)
-    if (!sheets) sheets = [...document.styleSheets];
-    else if (sheets.sup){    // sheets is a string
+    try {
+      // get stylesheet(s)
+      if (!sheets) sheets = [...document.styleSheets];
+      else if (sheets.sup){    // sheets is a string
         let absoluteURL = new URL(sheet, document.baseURI).href;
         sheets = [...document.styleSheets].filter(i => i.href == absoluteURL);
         }
-    else sheets = [sheets];  // sheets is a stylesheet
-        
-    // CSS (& HTML) reduce spaces to one. TODO: ignore quoted spaces.
-    selector = selector.replace(/\s+/g, ' ');
-    const findRule = s => [...s.cssRules].reverse().find(i => i.selectorText == selector)
-    let rule = sheets.map(findRule).filter(i=>i).pop()
+      else sheets = [sheets];  // sheets is a stylesheet
+          
+      // CSS (& HTML) reduce spaces to one. TODO: ignore quoted spaces.
+      selector = selector.replace(/\s+/g, ' ');
+      const findRule = s => [...s.cssRules].reverse().find(i => i.selectorText == selector)
+      let rule = sheets.map(findRule).filter(i=>i).pop()
 
-    const propsArr = props.sup 
+      const propsArr = props.sup 
         ? props.split(/\s*;\s*/).map(i => i.split(/\s*:\s*/)) // from string
         : Object.entries(props);                              // from Object
-            
-    if (rule) for (let [prop, val] of propsArr)
+              
+      if (rule) for (let [prop, val] of propsArr)
         rule.style[prop] = val;
-    else {
+      else {
         sheet = sheets.pop();
         if (!props.sup) props = propsArr.reduce((str, [k, v]) => `${str}; ${k}: ${v}`, '');
         sheet.insertRule(`${selector} { ${props} }`, sheet.cssRules.length);
       }
+    } catch(error) {
+      console.log(`Stylesheets cannot be changed in this environment. Some resizing might not work.\nError: ${error}`);
+    }
   }
 
   // === Rendering
